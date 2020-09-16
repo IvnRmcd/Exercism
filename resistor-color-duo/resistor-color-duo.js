77cd4ca3b92afe8c083e77b80f0986c77ca23ea2
Ivan Cox <ivn.cox@gmail.com>
2020-09-16 11:12:13 -0400

    Modify code to give the Function one job

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,11 +1,11 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const colors = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"].map(items => items.toLowerCase())
 
+const getIndex = (arrayList) => {
+    return arrayList.slice(0, 2)
+}
 
 export const decodedValue = (list) => {
-    const colors = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"].map(items => items.toLowerCase())
-    let colorCode = Number(list.slice(0, 2).map(value => colors.indexOf(value)).join(''))
-    return colorCode
+    return Number(getIndex(list)
+        .map(value => colors.indexOf(value))
+        .join(''))
 };
